3bc4ea9a68cb89d1849639ba79337c659c752c39
Stéphane <stephane.pouget@ens-lyon.fr>
2018-03-03 17:36:34 +0100

    indentation

--- dbdm.ml
+++ dbdm.ml
@@ -5,10 +5,10 @@ let appartient t1 l1 = match l1 with (*t1 dic*)
 
 let union l1 l2 = (*l1 et l2 liste de dictionnaire*)
   let rec inter l1 l2 l = match l2 with
-  | [] -> l
-  | t::q when (appartient t l1) -> inter l1 q t::l
-  | t::q -> inter l1 q l
-in inter l1 l2 []
+    | [] -> l
+    | t::q when (appartient t l1) -> inter l1 q t::l
+    | t::q -> inter l1 q l
+  in inter l1 l2 []
 
 let minus l1 l2 =
   let rec inter l1 l2 l = match l2 with

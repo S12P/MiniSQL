02e0656ab4f102d8afacc8b7c6af92a9a22a9624
Guillaume <guillaume@pc-63.home>
2018-03-04 10:29:17 +0100

    correction erreur

--- minisql.ml
+++ minisql.ml
@@ -2,7 +2,7 @@ open Csv
 open DataType
 
 
-let read_data () =
+let _ =
     let argc = Array.length Sys.argv in
     let tables = StringMap.empty in  (* dictionnaire de toutes les tables *)
     for i = 1 to argc - 1 do
@@ -11,13 +11,6 @@ let read_data () =
         let csvfile = Table.from_csv (Csv.load_in file) in
         let tables = StringMap.add file_name csvfile tables in
         print_string ("File " ^ file_name ^ ".csv loaded");
-        
+        Pervasives.close_in file
     done;
 
-
-
-let _ =
-    read_date ();
-    
-    
-    

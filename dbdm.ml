73e4ef4e886f2d65ffe9e778b654d31298a8529b
Stéphane <stephane.pouget@ens-lyon.fr>
2018-03-07 09:58:56 +0100

    correction

--- dbdm.ml
+++ dbdm.ml
@@ -2,12 +2,12 @@ open DataType
 
 type op = Eq | Lt | Gt
 
-type idstring = ID of string * string
+type idstring = ID of (string StringMap list) * string
       |  String of string
 
 type cond = And of cond * cond
         | Or of cond * cond
-        | Rel of string * op * string
+        | Rel of idstring * op * idstring
         | In of string * string list
 
 type requete = {col: string list; table: string list; cond: cond}
@@ -34,34 +34,35 @@ let appartient2 x l1 = match l1 with (*fonction utile pour le In dans test_cond*
   |t::q -> StringMap.exists (fun key elmt -> x=elmt) t
 
 
-let rec test_cond nbligne cond : bool = (*permet de tester la condition du where*)
+let rec test_cond table nbligne cond : bool = (*permet de tester la condition du where*)
     match cond with
-      | And (c1, c2) -> (test_cond nbligne c1 ) && (test_cond nbligne c2 )
-      | Or(c1, c2) -> (test_cond nbligne c1 ) || (test_cond nbligne c2 )
+      | And (c1, c2) -> (test_cond table nbligne c1 ) && (test_cond table nbligne c2 )
+      | Or(c1, c2) -> (test_cond table nbligne c1 ) || (test_cond table nbligne c2 )
       | Rel(s1, Eq, s2) -> (match s1 with
-                          | ID (table, colonne) -> table[nbligne].colonne
+                          | ID (table, colonne) ->  (List.nth table nbligne).colonne
                           | String(x) -> x
-                          ) =
+                          )
+                          =
                           (match s2 with
-                            | ID (table, colonne) -> table[nbligne].colonne (* valeur dans la base de données *)
+                            | ID (table, colonne) -> (List.nth table nbligne).colonne (* valeur dans la base de données *)
                             | String(x) -> x
                           )
       | Rel(s1, Lt, s2) -> (try let x = (match s1 with
-                          | ID (table, colonne) -> table[nbligne].colonne
+                          | ID (table, colonne) -> t(List.nth table nbligne).colonne
                           | String(x) -> x
                           ) in int_of_string x with _ -> x)
                           <
                           (try let x = (match s2 with
-                          | ID (table, colonne) -> table[nbligne].colonne
+                          | ID (table, colonne) -> (List.nth table nbligne).colonne
                           | String(x) -> x
                           ) in int_of_string x with _ -> x)
       | Rel(s1, Gt, s2) -> (try let x = (match s1 with
-                          | ID (table, colonne) -> table[nbligne].colonne
+                          | ID (table, colonne) -> (List.nth table nbligne).colonne
                           | String(x) -> x
                           ) in int_of_string x with _ -> x)
                           >
                           (try let x = (match s2 with
-                          | ID (table, colonne) -> table[nbligne].colonne
+                          | ID (table, colonne) -> (List.nth table nbligne).colonne
                           | String(x) -> x
                           ) in int_of_string x with _ -> x)
       | In (s, l) -> if appartient2 s l then
@@ -79,7 +80,7 @@ let where col table cond =
                             | co::autreco -> let rec inter3 co table cond numll l = match cond with
                               | true -> l @ [table[numll].co] ; inter3 autreco table cond numll l
                               | false -> inter3 autreco table cond numll l
-                            in inter3 co table (test_cond numl cond) numl l
+                            in inter3 co table (test_cond table numl cond) numl l
                         in inter2 col table cond numligne l
                       in inter11 col tab cond ((List.length tab)-1) l
   in inter1 col table cond []

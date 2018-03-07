da5a8e9557698610c82384db62b3408d17d09d3a
Stéphane <stephane.pouget@ens-lyon.fr>
2018-03-07 16:25:11 +0100

    modification car base de donnée est un dic de table. LE WHERE pas sur..

--- dbdm.ml
+++ dbdm.ml
@@ -1,16 +1,7 @@
 open DataType
 
-type op = Eq | Lt | Gt
-
-type idstring = ID of (string StringMap.t list) * string (*table colonne*)
-      |  String of string
-
-type cond = And of cond * cond
-        | Or of cond * cond
-        | Rel of idstring * op * idstring
-        | In of string * (string StringMap.t list)
-
-type requete = {col: string list; table: string list; cond: cond}
+(*on va avoir un dictionnaire de liste de dictionnaire ie un dictionnaire de tt les tables*)
+let avoir_table table base_de_donnee : string StringMap.t list = StringMap.find table base_de_donnee
 
 
 let appartient t1 l1 = match l1 with (*fonction utile pour union et minus avec t1 un dictionnaire et l1 une liste de dictionnaire*)
@@ -34,43 +25,31 @@ let appartient2 x l1 = match l1 with (*fonction utile pour le In dans test_cond*
   |t::q -> StringMap.exists (fun key elmt -> x=elmt) t
 
 
-let rec test_cond table nbligne cond : bool = (*permet de tester la condition du where*)
+let rec test_cond base_de_donnee nbligne cond : bool = (*permet de tester la condition du where*)
     match cond with
-      | And (c1, c2) -> (test_cond table nbligne c1 ) && (test_cond table nbligne c2 )
-      | Or(c1, c2) -> (test_cond table nbligne c1 ) || (test_cond table nbligne c2 )
+      | And (c1, c2) -> (test_cond base_de_donnee nbligne c1 ) && (test_cond base_de_donnee nbligne c2 )
+      | Or(c1, c2) -> (test_cond base_de_donnee nbligne c1 ) || (test_cond base_de_donnee nbligne c2 )
       | Rel(s1, Eq, s2) -> (match s1 with
-                          | ID (table, colonne) -> StringMap.find colonne (List.nth table nbligne)
+                          | ID (table, colonne) -> let table = (avoir_table table base_de_donnee) in StringMap.find colonne (List.nth table nbligne)
                           | String(x) -> x
                           )
                           =
                           (match s2 with
-                            | ID (table, colonne) -> StringMap.find colonne (List.nth table nbligne)  (* valeur dans la base de données *)
+                            | ID (table, colonne) -> let table = (avoir_table table base_de_donnee) in StringMap.find colonne (List.nth table nbligne)  (* valeur dans la base de données *)
                             | String(x) -> x
                           )
       | Rel(s1, Lt, s2) -> ((*try*) let x = (match s1 with
-                          | ID (table, colonne) -> StringMap.find colonne (List.nth table nbligne)
+                          | ID (table, colonne) -> let table = (avoir_table table base_de_donnee) in StringMap.find colonne (List.nth table nbligne)
                           | String(x) -> x
                           ) in int_of_string x(*) with _ -> s1)*))
                           <
                           ((*try*) let x = (match s2 with
-                          | ID (table, colonne) -> StringMap.find colonne (List.nth table nbligne)
+                          | ID (table, colonne) -> let table = (avoir_table table base_de_donnee) in StringMap.find colonne (List.nth table nbligne)
                           | String(x) -> x
                           ) in int_of_string x (*with _ -> s2)*))
-      | Rel(s1, Gt, s2) -> ((*try*) let x = (match s1 with
-                          | ID (table, colonne) -> StringMap.find colonne (List.nth table nbligne)
-                          | String(x) -> x
-                          ) in int_of_string x (*with _ -> s1*))
-                          >
-                          ((*try*) let x = (match s2 with
-                          | ID (table, colonne) -> StringMap.find colonne (List.nth table nbligne)
-                          | String(x) -> x
-                          ) in int_of_string x (*with _ -> s2)*))
-      | In (s, l) -> if appartient2 s l then
-                          true
-                      else false
 
 
-let where col table cond =
+let where_int col table cond =
   let rec inter1 col table cond l = match table with
     | [] -> l
     | tab::autretable -> let rec inter11 col table cond numligne l = match numligne with
@@ -84,6 +63,10 @@ let where col table cond =
                         in inter2 col table cond numligne l
                       in inter11 col tab cond ((List.length tab)-1) l
   in inter1 col table cond []
+
+
+let where col base_de_donnee cond = match col with
+  | (a, b)::q -> let table = (avoir_table a base_de_donnee); where_int b table cond
 
 
 (*let select entete csv requete =

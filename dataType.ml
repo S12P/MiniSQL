b6bddccb33456a256efb8dceb0b2445082c5735e
Stéphane <stephane.pouget@ens-lyon.fr>
2018-03-08 09:14:04 +0100

    suppresion dbdm

--- dataType.ml
+++ dataType.ml
@@ -5,16 +5,16 @@ module StringTable =
         type t = string
         let compare = compare
     end
-    
+
 module StringMap = Map.Make(StringTable)
-         
+
 
 
 type op = Eq (*| Lt*)
 
 type idstring = ID of string * string
 
-type column = Col of idstring 
+type column = Col of idstring
             | Rename of idstring * string
 
 type cond =
@@ -29,18 +29,11 @@ type requeteWhere =
      table: string list;     (* table dans le join *)
      cond: cond;         (* condition dans le where *)
     }
-            
-type requete = 
+
+type requete =
     | Where of requeteWhere
     | Union of requete * requete
     | Minus of requete * requete
-
-
-
-
-
-
-
 
 
 
@@ -48,8 +41,8 @@ type requete =
 module Table = struct
 
     type t = {row: string StringMap.t list; head: string array}
-    
-    
+
+
     (* Affiche du mieux possible une table *)
     let print_table table =
         let print_entete l =
@@ -59,16 +52,16 @@ module Table = struct
             done;
             print_newline ()
         in
-        
-        let print_elt l = 
+
+        let print_elt l =
             Array.iter (fun elt-> print_string ((StringMap.find elt l) ^ "  ")) table.head;
             print_newline ()
         in
         print_entete table.head;
         List.iter print_elt table.row
-    
-    
-    
+
+
+
     (* Crée une table à partir d'un CSV *)
     let from_csv csv table_name =
         let rec from_list llabels lvalue =
@@ -77,7 +70,7 @@ module Table = struct
             | _, [] -> StringMap.empty
             | t1::q1, t2::q2 -> StringMap.add (table_name ^ "." ^ t1) t2 (from_list q1 q2)
         in
-        let rec aux lvalues llabels= 
+        let rec aux lvalues llabels=
             match lvalues with
             | [] -> []
             | t::q -> (from_list llabels t) :: (aux q llabels)
@@ -85,15 +78,15 @@ module Table = struct
         match csv with
         | t::q -> {row = aux q t ; head = Array.of_list (List.map (fun x -> table_name ^ "." ^ x) t) }
         | _ -> failwith "Error with the CSV file\n"
-        
-    
-    
-    
+
+
+
+
     (* Un élément appartient-il à une table *)
     let appartient elt table =
          List.exists (fun x -> StringMap.equal (fun a b -> a = b) elt x) table
-     
-     
+
+
     (* Egalité entre 2 tableux *)
     let array_eq t1 t2 =
         if Array.length t1 <> Array.length t2 then false
@@ -105,10 +98,10 @@ module Table = struct
             done;
             !b
           end
-          
-          
-     
-    (* Union de deux tables *)    
+
+
+
+    (* Union de deux tables *)
     let union (t1 : t) (t2 : t) : t =
         let rec aux t1 t2 =
             match t2 with
@@ -119,9 +112,9 @@ module Table = struct
         if array_eq t1.head t2.head then
             { row = aux t1.row t2.row ; head = t1.head }
         else failwith "Union impossible"
-        
-        
-    (* Différence de deux tables *)    
+
+
+    (* Différence de deux tables *)
     let minus (t1 : t) (t2 : t) : t =
         let rec aux t1 t2 =
             match t2 with
@@ -132,10 +125,10 @@ module Table = struct
         if array_eq t1.head t2.head then
             { row = aux t1.row t2.row ; head = t1.head }
         else failwith "Minus impossible"
-        
-        
-        
-        
+
+
+
+
     let rec test_cond line cond : bool = (*permet de tester la condition du where*)
         match cond with
           | And (c1, c2) -> (test_cond line c1 ) && (test_cond line c2 )
@@ -146,10 +139,10 @@ module Table = struct
                               true
                           else false
             *)
-    
-    
+
+
         (* produit cartésien de 2 tables *)
-    let rec reduce_table table1 table2 = 
+    let rec reduce_table table1 table2 =
         let rec union elt1 elt2 =
             StringMap.fold (fun x y m -> StringMap.add  x y m) elt2 elt1
         in
@@ -164,16 +157,16 @@ module Table = struct
             | t::q -> (add t t2)@(unify q t2)
         in
         {row = unify table1.row table2.row; head = Array.append table1.head table2.head}
-    
+
     (* produit cartésien d'une liste de tables *)
     let rec reduce_table_list ltable = match ltable with
         | [] -> failwith "erreur"
         | [t] -> t
         | t1::t2::q -> reduce_table_list ((reduce_table t1 t1)::q)
-    
-    
-    
-    
+
+
+
+
     (* Renome une table, typiquement quand on a FILENALE ID, on renome la table FILENAME en ID *)
     let rename_table (table : t) (nom : string) =
         let a = Array.make (Array.length table.head) "" in
@@ -184,14 +177,14 @@ module Table = struct
         let rec renamerow l =
             match l with
             | [] -> []
-            | t::q -> (StringMap.fold (fun key elt m -> 
+            | t::q -> (StringMap.fold (fun key elt m ->
                                            let colname = String.sub key (String.index key '.' + 1) (String.length key - 1) in
                                             StringMap.add (nom ^ "." ^ colname) elt (StringMap.remove key m)) t t) :: (renamerow q)
         in
         {head = a; row = renamerow table.row}
-                        
-        
-    (* Change le nom d'une colonne lorsque l'on fait un SELECT A.col AS new_name *)    
+
+
+    (* Change le nom d'une colonne lorsque l'on fait un SELECT A.col AS new_name *)
     let rename_col (table : t) (col : string) (new_name : string) : t =
         let tablename = String.sub table.head.(0) 0 (String.index table.head.(0) '.') in
         let a = Array.make (Array.length table.head) "" in
@@ -201,8 +194,8 @@ module Table = struct
             else
                 a.(i) <- table.head.(i)
         done;
-        
-        let renameelt key elt m = 
+
+        let renameelt key elt m =
             if key = col then
                 StringMap.add (tablename ^ "." ^ new_name) elt (StringMap.remove key m)
             else
@@ -214,17 +207,17 @@ module Table = struct
             | t::q -> (StringMap.fold renameelt t t) :: (renamerow q)
         in
         {head = a; row = renamerow table.row}
-        
-        
-        
-        
-        
-        
-        
-        
-    (* Selection de colonnes dans une table selon une table selon une condition 
-    On va hacker en fait c'est plus facile ne ne modifiant que le champ head *) 
-    let select (col : column list) (tab : string list) (cond : cond) ltable : t = 
+
+
+
+
+
+
+
+
+    (* Selection de colonnes dans une table selon une table selon une condition
+    On va hacker en fait c'est plus facile ne ne modifiant que le champ head *)
+    let select (col : column list) (tab : string list) (cond : cond) ltable : t =
         let liste_table = List.map (fun x ->  StringMap.find x ltable) tab in
         let table = reduce_table_list liste_table in
         let head = Array.of_list (List.map (fun x -> match x with | Col(ID(a, b)) -> a ^ "." ^ b
@@ -232,40 +225,27 @@ module Table = struct
                                            ) col) in
         let row = List.filter (fun x -> test_cond x cond) table.row in
         let newtable = {head = head ; row = row} in
-        List.fold_right (fun a b -> match a with 
+        List.fold_right (fun a b -> match a with
                                     | Col(ID(_, _)) -> b
                                     | Rename(ID(t,c), new_name) -> rename_col b (t ^ "." ^ c) new_name)
                           col newtable
-          
-                          
-                          
-                          
-                          
-                          
-                          
-                          
+
+
+
+
+
+
+
+
 end
 
 
 
 
-            
-            
+
+
 let rec compute ast ltable =
     match ast with
     | Where({col = x; table = y; cond = z}) -> Table.select x y z ltable
     | Union(ast1, ast2) -> Table.union (compute ast1 ltable) (compute ast2 ltable)
     | Minus(ast1, ast2) -> Table.minus (compute ast1 ltable) (compute ast2 ltable)
-
-            
-
-
-
-
-
-
-
-
-
-
-            
